c9290058e2a1c5fd5f1a749111f853f417f30ce8
timlin <hchang.hc.lin@foxconn.com>
2016-12-16 14:41:28 +0800

    mtc code

--- rootfs-4.4-patch/mtc/lp5562_test.sh
+++ rootfs-4.4-patch/mtc/lp5562_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-retry=3
+#retry=3
 
 if [ "${1}" == "on" ]; then
 #blinking USER-R
@@ -8,47 +8,47 @@ echo 1 > /sys/bus/i2c/devices/4-0030/select_engine
 echo "RGB" > /sys/bus/i2c/devices/4-0030/engine_mux
 	until [ -e /sys/class/firmware/lp5562 ]
 	do
-	retry = $(($retry-1))
-	if [ $retry == "0" ]; then
-	  echo "RETRY"
-	  exit 0
-	fi
+	#retry = $(($retry-1))
+	#if [ $retry == "0" ]; then
+	#  echo "RETRY"
+	#  exit 0
+	#fi
 	sleep 0.1
 	done
 echo 1 > /sys/class/firmware/lp5562/loading
 echo "40FF5800E00440007000" > /sys/class/firmware/lp5562/data
 echo 0 > /sys/class/firmware/lp5562/loading
 
-retry=3
+#retry=3
 
 #blinking USER-G after UER-R
 echo 2 > /sys/bus/i2c/devices/4-0030/select_engine
 echo "RGB" > /sys/bus/i2c/devices/4-0030/engine_mux
 	until [ -e /sys/class/firmware/lp5562 ]
 	do
-	retry = $(($retry-1))
-	if [ $retry == "0" ]; then
-	  echo "RETRY"
-	  exit 0
-	fi
+	#retry = $(($retry-1))
+	#if [ $retry == "0" ]; then
+	#  echo "RETRY"
+	#  exit 0
+	#fi
 	sleep 0.1
 	done
 echo 1 > /sys/class/firmware/lp5562/loading
 echo "E08040FF5800E00840007000" > /sys/class/firmware/lp5562/data
 echo 0 > /sys/class/firmware/lp5562/loading
 
-retry=3
+#retry=3
 
 #blinking USER-B after UER-G
 echo 3 > /sys/bus/i2c/devices/4-0030/select_engine
 echo "RGB" > /sys/bus/i2c/devices/4-0030/engine_mux
 	until [ -e /sys/class/firmware/lp5562 ]
 	do
-	retry = $(($retry-1))
-	if [ $retry == "0" ]; then
-	  echo "RETRY"
-	  exit 0
-	fi
+	#retry = $(($retry-1))
+	#if [ $retry == "0" ]; then
+	#echo "RETRY"
+	#  exit 0
+	#fi
 	sleep 0.1
 	done
 echo 1 > /sys/class/firmware/lp5562/loading
