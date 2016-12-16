cfd9629d75aa3a7b40596ea385648d9ec6da7925
timlin <hchang.hc.lin@foxconn.com>
2016-12-16 14:12:13 +0800

    update mtc code

--- rootfs-4.4-patch/mtc/calibrate.sh
+++ rootfs-4.4-patch/mtc/calibrate.sh
@@ -2,8 +2,7 @@
 
 if [ "$1" == "once" ]; then
 #calibrate once
-	aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
-	sleep 1
+	aplay -P /mtc/silent_1sec.wav &
 	str=$(climax -dsysfs --slave=0x34 -r 0x80|grep 0x0413)
 	str1=$(climax -dsysfs --slave=0x34 -r 0x80|grep 0x0013)
 	
@@ -38,6 +37,17 @@ elif [ "$1" == "dump" ]; then
 	aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
 	sleep 1
 	climax -dsysfs --slave=0x34 -l /mtc/mono-ili.cnt --dump
+	
+elif [ "$1" == "check" ]; then
+#calibrate check
+	aplay -P /mtc/silent_1sec.wav &
+	str=$(climax -dsysfs --slave=0x34 -r 0x80|grep 0x0413)
+	str1=$(climax -dsysfs --slave=0x34 -r 0x80|grep 0x0013)
+	if [ ${#str} != "0" ] || [ ${#str1} != "0" ]; then
+		echo "Calibrated"
+	else
+		echo "K_Fail"
+	fi
 	
 fi
 
