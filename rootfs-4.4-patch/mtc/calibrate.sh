af9c4c7d21014f627e884e5bf6ecef6ddb458143
timlin <hchang.hc.lin@foxconn.com>
2017-05-12 08:31:23 +0800

    revised rootfs-4.4 mtc tools

--- rootfs-4.4-patch/mtc/calibrate.sh
+++ rootfs-4.4-patch/mtc/calibrate.sh
@@ -12,7 +12,7 @@ if [ "$1" == "once" ]; then
 	else
 		echo "Calibrating..."
 		aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
-		sleep 1
+		sleep 2
 		climax -dsysfs --slave=0x34 -l /mtc/mono-ili.cnt --calibrate=once
 		sleep 5
 		echo "Done"
@@ -21,7 +21,7 @@ if [ "$1" == "once" ]; then
 elif [ "$1" == "always" ]; then
 #calibrate always
 	aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
-	sleep 1
+	sleep 2
 	climax -dsysfs --slave=0x34 -l /mtc/mono-ili.cnt --calibrate
 	sleep 5
 	echo "Done"
@@ -29,13 +29,13 @@ elif [ "$1" == "always" ]; then
 elif [ "$1" == "show" ]; then
 #calibrate show
 	aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
-	sleep 1
+	sleep 2
 	climax -dsysfs --slave=0x34 -l /mtc/mono-ili.cnt --calshow
 	
 elif [ "$1" == "dump" ]; then
 #calibrate dump
 	aplay -P /mtc/1kHz_44100Hz_16bit_05sec.wav &
-	sleep 1
+	sleep 2
 	climax -dsysfs --slave=0x34 -l /mtc/mono-ili.cnt --dump
 	
 elif [ "$1" == "check" ]; then
